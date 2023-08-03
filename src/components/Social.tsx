bb790432b04dd7712c798b4f6c702a89119a8e6a
Adam Wilczek <adamwilczek@pragmative.io>
2023-08-03 13:21:04 +0200

    contact forms

--- src/components/Social.tsx
+++ src/components/Social.tsx
@@ -9,20 +9,20 @@ function Social() {
       <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
         <ul>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-sky-950'>
-            <a className='flex justify-between items-center w-full text-sky-200' href='www.google.com'>
+            <a className='flex justify-between items-center w-full text-gray-200' href='https://www.linkedin.com/in/adam-wilczek'>
               Linkendin <FaLinkedin size={30} />
             </a>
           </li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-green-950'>
-            <a className='flex justify-between items-center w-full text-sky-200' href='https://github.com/Wirux'>
+            <a className='flex justify-between items-center w-full text-gray-200' href='https://github.com/Wirux'>
               GitHub <FaGithub size={30} />
             </a></li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-purple-950'>
-            <a className='flex justify-between items-center w-full text-sky-200' href='/'>
+            <a className='flex justify-between items-center w-full text-gray-200' href='mailto: Adam.Wilczek@protonmail.com'>
               Email <HiOutlineMail size={30} />
             </a></li>
           <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-200 bg-gray-800'>
-            <a className='flex justify-between items-center w-full text-sky-200' href='/'>
+            <a className='flex justify-between items-center w-full text-gray-200' href='/'>
               Resume <BsFillPersonFill size={30} />
             </a></li>
         </ul>
@@ -34,15 +34,15 @@ function Social() {
           </p>
         </div>
         <div className='w-full grid grid-cols-2 gap-4 text-center py-8 '>
-          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-sky-700' href='google.com'>
+          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-sky-700' href='https://www.linkedin.com/in/adam-wilczek'>
             <FaLinkedin size={40} />
             <p>Linkendin</p>
           </a>
-          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-green-700'>
+          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-green-700' href='https://github.com/Wirux'>
             <FaGithub size={40} />
             <p>GitHub</p>
           </a>
-          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-purple-700' href='google.com'>
+          <a className='shadow-md shadow-[#040c16] hover:scale-110 hover:shadow-pink-950 duration-300 flex flex-col items-center justify-center text-center text-purple-700' href='mailto: Adam.Wilczek@protonmail.com'>
             <HiOutlineMail size={40} />
             <p>Email</p>
           </a>
